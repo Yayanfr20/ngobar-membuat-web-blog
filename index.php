16361e4d659e88d914b4207962b4c993374c9a04
yayanfr20 <yayanfathurohman20@gmail.com>
2023-03-28 22:08:53 +0700

    Membuat single page

--- index.php
+++ index.php
@@ -5,51 +5,50 @@
      <meta charset="UTF-8">
      <meta http-equiv="X-UA-Compatible" content="IE=edge">
      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Blogger - Home</title>
-     <link rel="stylesheet" href="css/index.css">
+     <title>Jals blog - Home</title>
+     <link rel="stylesheet" href="index.css">
      <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
 </head>
 
 <body>
      <!-- start navbar -->
-     <div class="container">
+     <div class="container box-navbar">
           <div class="navbar">
-               <p class="brand">Blogger</p>
+               <p class="brand">Jals blog</p>
                <div class="menu">
                     <a href=""><i class="bi bi-house"></i>Home</a>
                     <a href=""><i class="bi bi-plus-circle"></i>Post</a>
                </div>
           </div>
      </div>
+     <br><br><br><br>
      <!-- end navbar -->
      <div class="container">
           <div class="content">
                <div class="post">
                     <h2>Postingan Terbaru</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro eius, dolores deserunt
-                         vel inventore laboriosam nostrum dicta ea nisi aperiam ex, pariatur laudantium necessitatibus
-                         id. At, quam recusandae. Totam fuga dolorem eaque vitae? Illum culpa quibusdam optio eligendi,
-                         nisi nulla soluta voluptates eaque ullam earum natus exercitationem veritatis magnam corrupti
-                         eum laborum quas id. Adipisci, asperiores aspernatur. Ab nihil possimus quasi deserunt dolores
-                         minus atque, nulla nesciunt animi beatae voluptates, modi sequi est neque. Maxime cum dolore
-                         modi iste ea, libero quisquam veritatis iusto aperiam necessitatibus tempore dicta vitae neque
-                         repellendus autem adipisci facere. Saepe dolores corrupti at quam!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro eius, dolores deserunt
-                         vel inventore laboriosam nostrum dicta ea nisi aperiam ex, pariatur laudantium necessitatibus
-                         id. At, quam recusandae. Totam fuga dolorem eaque vitae? Illum culpa quibusdam optio eligendi,
-                         nisi nulla soluta voluptates eaque ullam earum natus exercitationem veritatis magnam corrupti
-                         eum laborum quas id. Adipisci, asperiores aspernatur. Ab nihil possimus quasi deserunt dolores
-                         minus atque, nulla nesciunt animi beatae voluptates, modi sequi est neque. Maxime cum dolore
-                         modi iste ea, libero quisquam veritatis iusto aperiam necessitatibus tempore dicta vitae neque
-                         repellendus autem adipisci facere. Saepe dolores corrupti at quam!</p>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro eius, dolores deserunt
-                         vel inventore laboriosam nostrum dicta ea nisi aperiam ex, pariatur laudantium necessitatibus
-                         id. At, quam recusandae. Totam fuga dolorem eaque vitae? Illum culpa quibusdam optio eligendi,
-                         nisi nulla soluta voluptates eaque ullam earum natus exercitationem veritatis magnam corrupti
-                         eum laborum quas id. Adipisci, asperiores aspernatur. Ab nihil possimus quasi deserunt dolores
-                         minus atque, nulla nesciunt animi beatae voluptates, modi sequi est neque. Maxime cum dolore
-                         modi iste ea, libero quisquam veritatis iusto aperiam necessitatibus tempore dicta vitae neque
-                         repellendus autem adipisci facere. Saepe dolores corrupti at quam!</p>
+                    <hr>
+                    <br>
+                    <div class="card">
+                         <img src="images/images1.jpeg" width="100%" height="250" alt="">
+                         <h4>Judul Postingan</h4>
+                         <div style="display: flex;">
+                              <p>Tanggal Postingan</p>
+                              <p>|</p>
+                              <p>Kategori Postingan</p>
+                         </div>
+                         <a href="read.php">Read more</a>
+                    </div>
+                    <div class="card">
+                         <img src="images/images1.jpeg" width="100%" height="200" alt="">
+                         <h4>Judul Postingan</h4>
+                         <div style="display: flex;">
+                              <p>Tanggal Postingan</p>
+                              <p>|</p>
+                              <p>Kategori Postingan</p>
+                         </div>
+                         <a href="">Read more</a>
+                    </div>
                </div>
                <div class="form-search">
                     <form action="">
---
+++ read.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Jals blog - Read</title>
+     <link rel="stylesheet" href="index.css">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+
+<body>
+     <!-- start navbar -->
+     <div class="container box-navbar">
+          <div class="navbar">
+               <p class="brand">Jals blog</p>
+               <div class="menu">
+                    <a href="index.php"><i class="bi bi-house"></i>Home</a>
+                    <a href=""><i class="bi bi-plus-circle"></i>Post</a>
+               </div>
+          </div>
+     </div>
+     <br><br><br><br>
+     <!-- end navbar -->
+     <div class="container">
+          <div class="content">
+               <div class="post">
+                    <h2>Judul Postingan</h2>
+                    <div style="display: flex;">
+                         <p>Tanggal Postingan</p>
+                         <p>|</p>
+                         <p>Kategori</p>
+                    </div>
+                    <br>
+                    <hr>
+                    <br>
+                    <img src="images/images1.jpeg" width="100%" height="250" alt="">
+                    <br><br>
+                    <hr>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ea voluptatibus libero voluptates
+                         incidunt culpa doloremque, recusandae saepe dolorum deserunt natus quam soluta facilis
+                         accusamus eum, cum ullam voluptatem ipsum dicta nam porro veritatis dolorem, at delectus! Cum
+                         cupiditate accusamus, debitis aut eaque mollitia ullam eum architecto, suscipit veritatis at.
+                         Dolores sit est sequi illum nam, at quia fugit? Possimus, consequatur impedit nostrum repellat
+                         recusandae ex cum velit, incidunt ipsam voluptate a enim delectus dolores illum aut nesciunt,
+                         quisquam beatae pariatur inventore veniam quam ducimus? Accusamus voluptatibus commodi,
+                         sapiente, ipsa officiis cum perspiciatis, tempore architecto iure ipsum rerum aliquam
+                         laudantium aperiam!</p>
+                    <br>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quas, adipisci! Adipisci corporis
+                         consectetur inventore illum harum voluptate ad deleniti, beatae fugiat. Optio in ratione
+                         explicabo laboriosam? Sequi minima amet ipsum.</p>
+                    <br>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa necessitatibus, illo quisquam
+                         laudantium sed dolor facilis assumenda. Soluta eaque officiis iusto qui porro sapiente illo
+                         amet placeat consequuntur officia ipsa corrupti aliquam, atque quibusdam at esse, sed nobis
+                         doloremque tempora repellendus itaque perspiciatis quia libero? Quae iure ut optio eveniet.</p>
+               </div>
+               <div class="form-search">
+                    <form action="">
+                         <input type="text" placeholder="Search">
+                         <button><i class="bi bi-search"></i></button>
+                    </form>
+               </div>
+          </div>
+     </div>
+</body>
+
+</html>
