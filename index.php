340530f23d810d6b8244011f5f47ad4e312bd553
yayanfr20 <yayanfathurohman20@gmail.com>
2023-03-28 21:37:36 +0700

    update page home

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+     <meta charset="UTF-8">
+     <meta http-equiv="X-UA-Compatible" content="IE=edge">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <title>Blogger - Home</title>
+     <link rel="stylesheet" href="css/index.css">
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
+</head>
+
+<body>
+     <!-- start navbar -->
+     <div class="container">
+          <div class="navbar">
+               <p class="brand">Blogger</p>
+               <div class="menu">
+                    <a href=""><i class="bi bi-house"></i>Home</a>
+                    <a href=""><i class="bi bi-plus-circle"></i>Post</a>
+               </div>
+          </div>
+     </div>
+     <!-- end navbar -->
+     <div class="container">
+          <div class="content">
+               <div class="post">
+                    <h2>Postingan Terbaru</h2>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro eius, dolores deserunt
+                         vel inventore laboriosam nostrum dicta ea nisi aperiam ex, pariatur laudantium necessitatibus
+                         id. At, quam recusandae. Totam fuga dolorem eaque vitae? Illum culpa quibusdam optio eligendi,
+                         nisi nulla soluta voluptates eaque ullam earum natus exercitationem veritatis magnam corrupti
+                         eum laborum quas id. Adipisci, asperiores aspernatur. Ab nihil possimus quasi deserunt dolores
+                         minus atque, nulla nesciunt animi beatae voluptates, modi sequi est neque. Maxime cum dolore
+                         modi iste ea, libero quisquam veritatis iusto aperiam necessitatibus tempore dicta vitae neque
+                         repellendus autem adipisci facere. Saepe dolores corrupti at quam!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro eius, dolores deserunt
+                         vel inventore laboriosam nostrum dicta ea nisi aperiam ex, pariatur laudantium necessitatibus
+                         id. At, quam recusandae. Totam fuga dolorem eaque vitae? Illum culpa quibusdam optio eligendi,
+                         nisi nulla soluta voluptates eaque ullam earum natus exercitationem veritatis magnam corrupti
+                         eum laborum quas id. Adipisci, asperiores aspernatur. Ab nihil possimus quasi deserunt dolores
+                         minus atque, nulla nesciunt animi beatae voluptates, modi sequi est neque. Maxime cum dolore
+                         modi iste ea, libero quisquam veritatis iusto aperiam necessitatibus tempore dicta vitae neque
+                         repellendus autem adipisci facere. Saepe dolores corrupti at quam!</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia porro eius, dolores deserunt
+                         vel inventore laboriosam nostrum dicta ea nisi aperiam ex, pariatur laudantium necessitatibus
+                         id. At, quam recusandae. Totam fuga dolorem eaque vitae? Illum culpa quibusdam optio eligendi,
+                         nisi nulla soluta voluptates eaque ullam earum natus exercitationem veritatis magnam corrupti
+                         eum laborum quas id. Adipisci, asperiores aspernatur. Ab nihil possimus quasi deserunt dolores
+                         minus atque, nulla nesciunt animi beatae voluptates, modi sequi est neque. Maxime cum dolore
+                         modi iste ea, libero quisquam veritatis iusto aperiam necessitatibus tempore dicta vitae neque
+                         repellendus autem adipisci facere. Saepe dolores corrupti at quam!</p>
+               </div>
+               <div class="form-search">
+                    <form action="">
+                         <input type="text" placeholder="Search">
+                         <button><i class="bi bi-search"></i></button>
+                    </form>
+               </div>
+          </div>
+     </div>
+</body>
+
+</html>
